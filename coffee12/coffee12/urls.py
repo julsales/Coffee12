b243e9c8e96af70ca5b89523c428528c22d15a91
Lucas Ferraz <134211506+Ferraz27@users.noreply.github.com>
2024-04-23 18:48:01 -0300

    Adição das telas de Login, Signup e Home
    commit para adicionar o codigo relacionado as telas de Login, Signup e Home

---
+++ coffee12/coffee12/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from coffee12app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('homepage/', views.Homepage, name='homepage'),  # Rota para a página inicial
+    path('signup/', views.SignupPage, name='signup'),  # Rota para a página de cadastro
+    path('', views.LoginPage, name='login'),
+    path('logout/', views.LogoutPage, name='logout'),  # Rota para a página de login
+]
