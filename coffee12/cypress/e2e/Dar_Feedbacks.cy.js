5a91368fd8e48f4799ac7f6fcda66fe2f0003660
Júlia Sales Novais <142419446+julsales@users.noreply.github.com>
2024-06-11 23:47:05 -0300

    Teste de dar e receber feedback

--- coffee12/cypress/e2e/Dar_Feedbacks.cy.js
+++ coffee12/cypress/e2e/Dar_Feedbacks.cy.js
@@ -1,6 +1,15 @@
 describe('Dar feedback', () => {
-    
-    it('', () => {
-        
+    it('Dar feedback a uma cafeteria', () => {
+        cy.visit('/');
+        cy.get('.button').click()
+        cy.get('#username').type('123')
+        cy.get('#password').type('123')
+        cy.get('button').click()
+        cy.get('.cafe > a').click()
+        cy.get('a.btn').should('be.visible').click()
+        cy.get('#id_rating').type('5')
+        cy.get('#id_comment').type('Muito bom amei')
+        cy.get('button').click()
         })
+
 })
